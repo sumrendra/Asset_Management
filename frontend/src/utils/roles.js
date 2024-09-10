f6bbb8e63c7cbd277c1c16d2a92c511d139628f5
b3yondzero <mohit.j.4730@gmail.com>
2024-09-10 11:06:06 -0400

    utility file for roles

---
+++ frontend/src/utils/roles.js
@@ -0,0 +1,5 @@
+export const roles = [
+    { label: 'Purchasing Department Role', roleString: 'PURCHASING_DEP_ROLE' },
+    { label: 'Maintenance Department Role', roleString: 'MAINTENANCE_DEP_ROLE' }
+  ];
+  
