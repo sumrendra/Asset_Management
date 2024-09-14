1c66ae0130b5cdeec0fec40ea7bdb24ee9aa2861
b3yondzero <mohit.j.4730@gmail.com>
2024-09-13 20:20:08 -0400

    updated admin middleware

--- backend/middleware/adminMiddleware.js
+++ backend/middleware/adminMiddleware.js
@@ -1,27 +1,22 @@
-const { ethers } = require('ethers');
-const UserRolesArtifact = require('../../frontend/src/contracts/UserRoles.json');
-const provider = new ethers.providers.JsonRpcProvider('http://127.0.0.1:8545'); // Verify this URL
-const contractAddress = require('../../frontend/src/contracts/contract-address.json');
-const contractABI = UserRolesArtifact.abi;
-const userRolesContract = new ethers.Contract(contractAddress.UserRoles, contractABI, provider);
+const RoleModel = require('../models/RoleModel');
+
 const checkAdmin = async (req, res, next) => {
+  const account = req.headers['account'];
+  if (!account) {
+    return res.status(400).json({ error: 'Account address is required' });
+  }
+
   try {
+    const isAdmin = await RoleModel.hasAdminRole(account);
     
-    const account = req.headers['account'];
-    if (!account) {
-      return res.status(400).json({ message: 'Account address is required' });
+    if (!isAdmin) {
+      return res.status(403).json({ error: 'Access denied. Admins only.' });
     }
 
-    // Check if account is admin
-    const isAdmin = await userRolesContract.hasRole(ethers.constants.HashZero, account);
-    if (isAdmin) {
-      next(); // Continue to the route handler
-    } else {
-      res.status(403).json({ message: 'Forbidden: Not an admin' });
-    }
+    next(); // Continue if the user is an admin
   } catch (error) {
-    console.error('Error in checkAdmin middleware:', error);
-    res.status(500).json({ message: 'Internal server error' });
+    console.error('Error in admin check middleware:', error);
+    res.status(500).json({ error: 'Internal server error' });
   }
 };
 
